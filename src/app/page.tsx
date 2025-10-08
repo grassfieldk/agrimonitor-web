34547d4cf88f1fb8512184b5441f916b79ea45ad
TAGUSA Kentaro <tagusa-k@grassfield.online>
2025-10-08 20:04:41 +0900

    画面のデザインを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { initialSensorData, type SensorData } from "@/types/types";
 
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 const fetchInterval = Number(process.env.NEXT_PUBLIC_FETCH_INTERVAL);
+const fetchIntervalSeconds = fetchInterval / 1000;
 
 export default function Home() {
   const [data, setData] = useState<SensorData>(initialSensorData);
@@ -27,7 +27,7 @@ export default function Home() {
       }
     };
     fetchData();
-    timer = setInterval(fetchData, fetchInterval * 1000);
+    timer = setInterval(fetchData, fetchInterval);
     return () => clearInterval(timer);
   }, []);
 
@@ -36,24 +36,78 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-neutral-800 rounded-lg shadow p-8">
-      <div className=" flex flex-col items-center gap-4">
-        <p>取得日時: {data.datetime}</p>
-        <p className="text-2xl">
-          {data.temperature}℃ / {data.humidity}%
-        </p>
-        {photoUrl && (
-          <div className="relative w-64 h-48">
-            <Image
-              src={photoUrl}
-              alt="camera"
-              fill
-              className="rounded-lg border border-neutral-700 object-contain"
-              priority
-            />
-          </div>
-        )}
+    <div className="p-4 flex flex-col justify-center items-center space-y-2 bg-neutral-800 rounded-lg shadow">
+      <div className="flex flex-row flex-wrap items-center mb-2 max-w-64 w-full justify-between mx-auto">
+        <DatetimeDisplay datetime={data.datetime} />
+        <TempHumidityDisplay
+          temperature={data.temperature}
+          humidity={data.humidity}
+        />
       </div>
+      <PhotoDisplay photoUrl={photoUrl} />
     </div>
   );
 }
+
+const DatetimeDisplay = ({ datetime }: { datetime: string }) => (
+  <div>
+    <span className="text-xs text-neutral-300">更新日時</span>
+    <br />
+    {datetime}
+  </div>
+);
+
+const TempHumidityDisplay = ({
+  temperature,
+  humidity,
+}: {
+  temperature: string;
+  humidity: string;
+}) => {
+  // 10-30: #3b82f6-#c41e1e
+  const tempNum = Math.min(Math.max(Number(temperature), 10), 30);
+  const ratio = (tempNum - 10) / 20;
+  const r = Math.round(59 + ratio * (200 - 59));
+  const g = Math.round(130 - ratio * (130 - 30));
+  const b = Math.round(246 - ratio * (246 - 30));
+  const tempColor = `rgb(${r},${g},${b})`;
+
+  // 40-70: #38bdf8-#3b82f6
+  const humNum = Math.min(Math.max(Number(humidity), 40), 70);
+  const humRatio = (humNum - 40) / 30;
+  const hr = Math.round(56 + humRatio * (59 - 56));
+  const hg = Math.round(189 + humRatio * (130 - 189));
+  const hb = Math.round(248 + humRatio * (246 - 248));
+  const humColor = `rgb(${hr},${hg},${hb})`;
+
+  return (
+    <div>
+      <span className="text-xl font-bold" style={{ color: tempColor }}>
+        {temperature}
+      </span>
+      <span className="inline-block w-6 text-center text-neutral-300">℃</span>
+      <br />
+      <span className="text-xl font-bold" style={{ color: humColor }}>
+        {humidity}
+      </span>
+      <span className="inline-block w-6 text-center text-neutral-300">%</span>
+    </div>
+  );
+};
+
+const PhotoDisplay = ({ photoUrl }: { photoUrl: string }) => {
+  if (!photoUrl) return null;
+  return (
+    <div className="relative w-full aspect-video">
+      <Image
+        src={photoUrl}
+        alt="camera"
+        fill
+        sizes="(max-width: 640px) 100vw, 640px"
+        className="rounded-lg border border-neutral-700 object-cover bg-black"
+        priority
+      />
+      {`※ 画像は ${fetchIntervalSeconds} 秒間隔で更新されます`}
+    </div>
+  );
+};
