3a08492584b04c12abf7e4cf2aa28746e0a4bc7a
TAGUSA Kentaro <tagusa-k@grassfield.online>
2025-10-23 23:05:12 +0900

    ナビゲーションバーがコンテンツの下に隠れないように修正

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const title = process.env.NEXT_PUBLIC_TITLE || "Agrimonitor";
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-foreground backdrop-blur-xs text-light shadow">
+    <nav className="fixed top-0 left-0 w-full bg-foreground backdrop-blur-xs text-light shadow z-10">
       <div className="max-w-screen-xl mx-auto flex items-center px-4 py-2">
         <div className="w-16">
           {!isTopPage && (
